17761c15fbce5fee3527ef33b8de8d794f066ee0
Dennis Ritter <dennis.r.ritter@gmail.com>
2021-12-20 18:34:55 +0100

    add run script

---
+++ scripts/run.sh
@@ -0,0 +1,10 @@
+python preprocessing.py \
+--metadata_file './data/drucker.xlsx' \
+--blender_file './data/drucker.blend' \
+--output_dir './out/test' \
+--n_images 5 \
+--camera_def_mode 'global-static' \
+--light_def_mode 'global-static' \
+--material_def_mode 'static' \
+--envmap_def_mode 'global-static' \
+
