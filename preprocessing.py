5399a62fae38152b13a1ecebb3b33c493df87648
Dennis Ritter <dennis.r.ritter@gmail.com>
2021-12-20 18:33:20 +0100

    add preprocessing start script

--- preprocessing.py
+++ preprocessing.py
@@ -1,34 +1,58 @@
+import sys
 import os
-import argparse
+from types import SimpleNamespace
+import logging
+import click
+
+from utils import logger_utils, timer_utils
+from preprocessing.preprocessing_controller import PreprocessingController
 
 
-def get_args():
-    parser = argparse.ArgumentParser()
+# yapf: disable
+# TODO: Improve option definitions -> metavar, show_default, type, required
+@click.command()
+@click.option('-metaf', '--metadata_file', help='Path to metadata file (machine-metadata.xlsx)')
+@click.option('-blendf', '--blender_file', help='Path to blender file (machine.blend)')
+@click.option('-out', '--output_dir', help='Output root directory', default='./gltf-export-configurations')
+@click.option('-n', '--n_images', help='Number of images to render for each part', default=10)
+@click.option('-cams', '--camera_def_mode', help='Camera definition mode', default='global-static')
+@click.option('-lights', '--light_def_mode', help='Light definition mode', default='global-static')
+@click.option('-mats', '--material_def_mode', help='Material definition mode', default='static')
+@click.option('-envmaps', '--envmap_def_mode', help='Environment Map definition mode', default='global-static')
+# yapf: enable
+def main(**kwargs):
+    args = SimpleNamespace(**kwargs)
 
-    # get all script args
-    _, all_arguments = parser.parse_known_args()
-    double_dash_index = all_arguments.index('--')
-    script_args = all_arguments[double_dash_index + 1:]
-
-    # add parser rules
-    parser.add_argument('-metaf', '--metadata_file', help='Path to metadata file (machine-metadata.xlsx)')
-    parser.add_argument('-blendf', '--blender_file', help='Path to blender file (machine.blend)')
-    parser.add_argument('-out', '--output_dir', help='Output root directory', default='./gltf-export-configurations')
-    parser.add_argument('-n', '--n_images', help='Number of images to render for each part', default=10)
-    parser.add_argument('-lights', '--light_sampling', help='Light sampling method', default='global-static')
-    parser.add_argument('-cams', '--camera_sampling', help='Camera sampling method', default='global-static')
-    parser.add_argument('-envmaps', '--environment_maps', help='Environment Map selection', default='global-static')
-    parser.add_argument('-mats', '--material_assign', help='Material selection', default='static')
-    parsed_script_args, _ = parser.parse_known_args(script_args)
-    return parsed_script_args
-
-if __name__ == '__main__':
-    args = get_args()
     metadata_file = args.metadata_file
     blender_file = args.blender_file
     output_dir = args.output_dir
     n_images = args.n_images
-    camera_sampling = args.camera_sampling
-    light_sampling = args.light_sampling
-    material_assign = args.material_assign
-    environment_maps = args.environment_maps
+    camera_def_mode = args.camera_def_mode
+    light_def_mode = args.light_def_mode
+    material_def_mode = args.material_def_mode
+    envmap_def_mode = args.envmap_def_mode
+
+    # Init Logger
+    LOGGER = logging.getLogger(__name__)
+    os.makedirs(output_dir, exist_ok=True)
+    logger_utils.init_logger(output_path=output_dir)
+
+    # Print run args
+    LOGGER.info('\n')
+    LOGGER.info('Preprocessing options:')
+    LOGGER.info(args)
+
+    ppc = PreprocessingController(
+        metadata_file,
+        blender_file,
+        output_dir,
+        n_images,
+        camera_def_mode,
+        light_def_mode,
+        material_def_mode,
+        envmap_def_mode,
+    )
+
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
