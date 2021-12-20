46986aeb40a6421b4193f9d150ca0d6010f4ce5a
Dennis Ritter <dennis.r.ritter@gmail.com>
2021-12-20 13:59:43 +0100

    Add preprocessing args

--- preprocessing.py
+++ preprocessing.py
@@ -13,16 +13,22 @@ def get_args():
     # add parser rules
     parser.add_argument('-metaf', '--metadata_file', help='Path to metadata file (machine-metadata.xlsx)')
     parser.add_argument('-blendf', '--blender_file', help='Path to blender file (machine.blend)')
-    parser.add_argument('-out', '--output_directory', help='Output root directory', default='./gltf-export-configurations')
+    parser.add_argument('-out', '--output_dir', help='Output root directory', default='./gltf-export-configurations')
     parser.add_argument('-n', '--n_images', help='Number of images to render for each part', default=10)
     parser.add_argument('-lights', '--light_sampling', help='Light sampling method', default='global-static')
     parser.add_argument('-cams', '--camera_sampling', help='Camera sampling method', default='global-static')
     parser.add_argument('-envmaps', '--environment_maps', help='Environment Map selection', default='global-static')
-    parser.add_argument('-mats', '--camera_sampling', help='Material selection', default='static')
+    parser.add_argument('-mats', '--material_assign', help='Material selection', default='static')
     parsed_script_args, _ = parser.parse_known_args(script_args)
     return parsed_script_args
 
 if __name__ == '__main__':
     args = get_args()
-    input_directory = args.input_directory
-    output_directory = args.output_directory
+    metadata_file = args.metadata_file
+    blender_file = args.blender_file
+    output_dir = args.output_dir
+    n_images = args.n_images
+    camera_sampling = args.camera_sampling
+    light_sampling = args.light_sampling
+    material_assign = args.material_assign
+    environment_maps = args.environment_maps
