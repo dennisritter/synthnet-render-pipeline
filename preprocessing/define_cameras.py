75510253bad49bddf66f5c36f57001b4cc993a0e
Dennis Ritter <dennis.r.ritter@gmail.com>
2021-12-23 17:22:34 +0100

    implement define_cameras for random sampling

--- preprocessing/define_cameras.py
+++ preprocessing/define_cameras.py
@@ -1,1 +1,13 @@
 """ Functions for camera definition """
+import preprocessing.utils.sampling as sampling
+from models.camera import Camera
+
+
+def get_cameras_random(n: int):
+    cam_positions = sampling.sphere_random(n)
+    return [Camera(position=cam_pos) for cam_pos in cam_positions]
+
+
+def get_cameras_uniform(n: int):
+    pass
+    # TODO: Add preprocessing.utils.sampling.sphere_uniform function
