72aad98af81487a15cd7ed7ceee50084a038b94f
Dennis Ritter <dritter@beuth-hochschule.de>
2022-01-03 12:26:45 +0100

    get list of lights func

--- preprocessing/define_lights.py
+++ preprocessing/define_lights.py
@@ -1,1 +1,19 @@
 """ Functions for light definition """
+from preprocessing.utils import sampling
+from models.light import Light
+import numpy as np
+
+
+def get_lights_range(
+        n: int,
+        xrange=(-1.0, 1.0),
+        yrange=(-1.0, 1.0),
+        zrange=(1.0, 1.0),
+):
+    light_positions = sampling.range_uniform(
+        n_samples=n,
+        xrange=xrange,
+        yrange=yrange,
+        zrange=zrange,
+    )
+    return [Light(position=light_pos.tolist()) for light_pos in light_positions]
