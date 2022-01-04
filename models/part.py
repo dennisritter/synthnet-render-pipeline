e2453a10ca8db70e2fe1a3ee3cc98469dea1f866
Dennis Ritter <dennis.r.ritter@gmail.com>
2022-01-04 16:27:42 +0100

    change part, scene class to follow ecfg_schema; rename global_scene to Scene

--- models/part.py
+++ models/part.py
@@ -15,9 +15,7 @@ class Part:
         hierarchy: str,
         is_spare: bool = False,
         single_parts: list = [],
-        # cameras: list = [],
-        # lights: list = [],
-        # envmaps: list = [],
+        scenes: list = [],
     ):
 
         ## Validate parameters
@@ -26,19 +24,15 @@ class Part:
         assert isinstance(hierarchy, str)
         assert isinstance(is_spare, bool)
         assert isinstance(single_parts, list)
-        # assert isinstance(cameras, list)
-        # assert isinstance(lights, list)
-        # assert isinstance(envmaps, list)
+        assert isinstance(scenes, list)
 
         ## Assign properties
         self.id = id
         self.name = name
         self.hierarchy = hierarchy
-        self.is_spare = is_spare
         self.single_parts = single_parts
-        # self.cameras = cameras
-        # self.lights = lights
-        # self.envmaps = envmaps
+        self.is_spare = is_spare
+        self.scenes = []
 
     def __eq__(self, other):
         if not isinstance(other, Part):
