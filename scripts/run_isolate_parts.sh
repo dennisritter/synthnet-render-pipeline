b234144b362768dbb8b88ec47b671c4863eb3804
Dennis Ritter <dennis.r.ritter@gmail.com>
2022-01-11 12:05:30 +0100

    update run script

--- scripts/run_isolate_parts.sh
+++ scripts/run_isolate_parts.sh
@@ -1,2 +1,6 @@
-blender -b -P isolate_parts.py -- --rcfg_file ./cfg/rcfg_test_v2.json --outpur_dir ./out
+blender data/drucker-example/drucker.blend \
+    --background \
+    --python isolate_parts.py \
+    -- --rcfg_file ./cfg/rcfg_test_v2.json \
+    --outpur_dir ./out
 
