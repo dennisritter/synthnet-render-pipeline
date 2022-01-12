69d1de6fb19f8dd3127c26e9b63ab9700d8d45cb
Dennis Ritter <dennis.r.ritter@gmail.com>
2022-01-12 02:12:02 +0100

    rename run script

---
+++ scripts/run_get_render_parts.sh
@@ -0,0 +1,2 @@
+blender data/drucker-example/drucker.blend --background --factory-startup --python get_render_parts.py -- --rcfg_file ./cfg/rcfg_v2_test.json --output_dir ./out
+
--- scripts/run_isolate_parts.sh
+++
@@ -1,2 +0,0 @@
-blender data/drucker-example/drucker.blend --background --factory-startup --python isolate_parts.py -- --rcfg_file ./cfg/rcfg_v2_test.json --output_dir ./out
-
