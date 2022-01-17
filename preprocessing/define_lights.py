8cad03f5c8de29c2c0f92ad8d61818e352fe3b1f
Dennis Ritter <dennis.r.ritter@gmail.com>
2022-01-17 16:07:59 +0100

    define_lights: docs, add uniform sampling

--- preprocessing/define_lights.py
+++ preprocessing/define_lights.py
@@ -3,12 +3,15 @@ from preprocessing.utils import sampling
 from models.light import Light
 
 
-def get_lights_range_uniform(
-        n: int,
-        xrange=(-1.0, 1.0),
-        yrange=(-1.0, 1.0),
-        zrange=(1.0, 1.0),
-):
+def get_lights_range_uniform(n: int, xrange=(-1.0, 1.0), yrange=(-1.0, 1.0), zrange=(1.0, 1.0)) -> list[Light]:
+    """ Returns a list of lights with positions sampled from the given ranges.
+
+        Args:
+            n (int): Number of lights
+            xrange (Tuple): Defines (min, max) range for axis
+            yrange (Tuple): Defines (min, max) range for axis
+            zrange (Tuple): Defines (min, max) range for axis
+    """
     light_positions = sampling.range_uniform(
         n_samples=n,
         xrange=xrange,
@@ -16,3 +19,13 @@ def get_lights_range_uniform(
         zrange=zrange,
     )
     return [Light(position=light_pos.tolist()) for light_pos in light_positions]
+
+
+def get_lights_sphere_uniform(n: int) -> list[Light]:
+    """ Returns a list of lights that were sampled on a sphere surface.
+
+        Args:
+            n (int): Number of lights to sample
+    """
+    light_positions = sampling.sphere_uniform(n)
+    return [Light(position=light_pos) for light_pos in light_positions]
