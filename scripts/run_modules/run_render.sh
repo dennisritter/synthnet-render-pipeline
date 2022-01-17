9a41237f1a5d020937453e947d7a15453d63cc32
Dennis Ritter <dennis.r.ritter@gmail.com>
2022-01-17 14:19:18 +0100

    remove single module run scripts. (need to much work to maintain)

--- scripts/run_modules/run_render.sh
+++
@@ -1,7 +0,0 @@
-INPUT_DIR=./out/export_gltf_test_n3
-OUTPUT_DIR=./out/render_test_n3
-
-
-blender --background --python ./render/render.py -- \
- --input_directory $INPUT_DIR \
- --output_directory $OUTPUT_DIR \
