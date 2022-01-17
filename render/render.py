7ce9eb1de5ea1294e2aa9a950a923d17e4045973
Dennis Ritter <dennis.r.ritter@gmail.com>
2022-01-17 14:56:05 +0100

    rename some args; add more type info to args, minor cleanup

--- render/render.py
+++ render/render.py
@@ -2,7 +2,6 @@
 import argparse
 import os
 import time
-# blender api
 import bpy
 
 
@@ -17,14 +16,13 @@ def load_gltf(file_path):
 def render(glb_fname, output_directory, args):
     # Scene setup
     scene = bpy.context.scene
-
     scene.render.engine = args.engine
     scene.render.image_settings.file_format = args.output_format
     scene.render.resolution_x = args.resolution_x
     scene.render.resolution_y = args.resolution_y
     scene.render.image_settings.quality = args.output_quality
-
     cameras = [obj for obj in scene.objects if obj.type == 'CAMERA']
+
     # render loop
     for i, cam in enumerate(cameras):
         bpy.context.scene.camera = cam
@@ -36,31 +34,67 @@ def render(glb_fname, output_directory, args):
 def get_args():
     parser = argparse.ArgumentParser()
 
-    # get all script args
+    parser.add_argument(
+        '--in_dir',
+        help="Directory with gltf files.",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        '--out_dir',
+        help="Directory to save the rendered images in.",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        '--res_x',
+        help="Pixel Resolution in X direction.",
+        default=512,
+        type=int,
+    )
+    parser.add_argument(
+        '--res_y',
+        help="Pixel Resolution in Y direction.",
+        default=512,
+        type=int,
+    )
+    parser.add_argument(
+        '--out_quality',
+        help="The output quality [0, 100].",
+        default=100,
+        type=int,
+        metavar="[0, 100]",
+        choices=range(0, 101),
+    )
+    parser.add_argument(
+        '--out_format',
+        help="Output image format",
+        default="JPEG",
+        type=str,
+        choices=["JPEG", "PNG"],
+    )
+    parser.add_argument(
+        '--engine',
+        help="Rendering engine",
+        default="CYCLES",
+        type=str,
+    )
+
+    # Only consider script args, ignore blender args
     _, all_arguments = parser.parse_known_args()
     double_dash_index = all_arguments.index('--')
     script_args = all_arguments[double_dash_index + 1:]
-
-    # add parser rules
-    parser.add_argument('-i', '--in_dir', help="Directory with gltf files.", type=str)
-    parser.add_argument('-o', '--out_dir', help="Directory to save the rendered images in.", type=str)
-    parser.add_argument('-rx', '--resolution_x', help="Resolution in X", default=512, type=int)
-    parser.add_argument('-ry', '--resolution_y', help="Resolution in Y", default=512, type=int)
-    parser.add_argument('-oq', '--output_quality', help="Output Quality in range[1, 100]", default=100, type=int)
-    parser.add_argument('-of', '--output_format', help="Output Format for images", default="JPEG", type=str)
-    parser.add_argument('-e', '--engine', help="rendering engine", default="CYCLES", type=str)
-    parsed_script_args, _ = parser.parse_known_args(script_args)
-    return parsed_script_args
+    args, _ = parser.parse_known_args(script_args)
+    return args
 
 
 if __name__ == '__main__':
+    tstart = time.time()
     args = get_args()
+    print(f"Running Rendering with args:\n{args}")
+
     in_dir = args.in_dir
     out_dir = args.out_dir
-
-    print(args)
-    tstart = time.time()
-
     sorted_input_files = sorted(os.listdir(in_dir), key=lambda x: x.split("_")[0])
 
     for glb_fname in sorted_input_files:
@@ -71,5 +105,4 @@ if __name__ == '__main__':
         render(glb_fname, out_dir, args)
 
     tend = time.time() - tstart
-
     print(f"Rendered {len(os.listdir(in_dir))} imgs in {tend} seconds")
