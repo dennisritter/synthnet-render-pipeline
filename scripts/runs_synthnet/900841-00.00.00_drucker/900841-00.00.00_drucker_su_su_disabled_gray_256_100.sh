980ac3f05379e37d378c26fc8617bab47d54db33
Dennis Ritter <dennis.r.ritter@gmail.com>
2022-04-04 15:47:46 +0200

    add run configs and kube script

--- scripts/runs_synthnet/900841-00.00.00_drucker/900841-00.00.00_drucker_su_su_disabled_gray_256_100.sh
+++ scripts/runs_synthnet/900841-00.00.00_drucker/900841-00.00.00_drucker_su_su_disabled_gray_256_100.sh
@@ -44,16 +44,16 @@ echo "- - - - - - - - - - - - - - - - - - - - "
 ##### RESOURCES
 # Resource directory path. Contains Environment maps, materials, CAD data and metadata. 
 # (relative to project root)
-RESOURCE_DIR="./data/236429-00.00.ST_Wolverine"
-TOPEX_METADATA_FILE="${RESOURCE_DIR}/236429-00.00.ST_Wolverine.xlsx"
-TOPEX_BLENDER_FILE="${RESOURCE_DIR}/236429-00.00.00_Wolverine.blend"
+RESOURCE_DIR="./data/900841-00.00.00_drucker"
+TOPEX_METADATA_FILE="${RESOURCE_DIR}/900841-00.00.00_drucker.xlsx"
+TOPEX_BLENDER_FILE="${RESOURCE_DIR}/900841-00.00.00_drucker.blend"
 MATERIALS_DIR="${RESOURCE_DIR}/materials"
 ENVMAPS_DIR="${RESOURCE_DIR}/envmaps"
 
 ##### OUTPUTS
 # Specify output root directory and a run description to create a unique output directory
 OUT_ROOT_DIR="./out"
-RUN_DESCRIPTION="236429-00.00.ST_Wolverine_se_su_static_static_256_12"
+RUN_DESCRIPTION="900841-00.00.00_drucker_su_su_disabled_gray_256_100"
 # will return a dir path like '$OUT_ROOT_DIR/$ID-$RUN_DESCRIPTION -> ./out/1-my-run
 OUT_DIR=`python scripts/utils/make_unique_out_dir.py "$OUT_ROOT_DIR" "$RUN_DESCRIPTION"`
 echo "Created output directory: $OUT_DIR"
@@ -62,10 +62,10 @@ echo "Created output directory: $OUT_DIR"
 # Set options
 N_IMAGES_PER_PART=12
 SCENE_MODE='exclusive'
-CAMERA_DEF_MODE='sphere-equidistant'
+CAMERA_DEF_MODE='sphere-uniform'
 LIGHT_DEF_MODE='sphere-uniform'
-MATERIAL_DEF_MODE='static'
-ENVMAP_DEF_MODE='static'
+MATERIAL_DEF_MODE='disabled'
+ENVMAP_DEF_MODE='gray'
 
 # Run Preprocessing
 PREPROCESSING_SECONDS_START=$SECONDS
@@ -150,7 +150,7 @@ if [[ $RUN_MODE -ge 3 ]]; then
     --render_format $OUT_FORMAT \
     --render_engine $ENGINE \
     --render_device $DEVICE \
-    --comment "Aluminium material = steel material; base color = 0.15, 0.15, 0.15, 1" 
+    --comment "No comment" 
 fi
 ############################
 
