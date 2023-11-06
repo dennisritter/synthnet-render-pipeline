d7e7eb823c2da61d7facbd1060a5834cf6af6273
Dennis Ritter <dennis.ritter@bht-berlin.de>
2023-11-06 16:01:18 +0100

    args minor

--- preprocessing.py
+++ preprocessing.py
@@ -100,7 +100,7 @@ def main(**kwargs):
 
     metadata_file = args.topex_metadata_file
     blend_file = args.topex_blend_file
-    materials_dir = args.topex_materials_dir
+    materials_dir = args.materials_dir
     obj_dir = args.obj_dir
     out_dir = args.out_dir
     n_images_per_part = args.n_images_per_part
